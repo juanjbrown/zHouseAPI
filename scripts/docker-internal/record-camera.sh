932a84ef77276119e2a247a0112b4d7cc8cd8587
pauljdehmer <paul@lisnr.com>
2016-02-19 01:19:10 -0500

    work

--- scripts/docker-internal/record-camera.sh
+++ scripts/docker-internal/record-camera.sh
@@ -4,6 +4,6 @@ cameraname=$1
 recordurl=$2
 duration=$3
 location=$4
-date=`date +%Y-%m-%d___%H-%M-%S`
+date=`date +%Y-%m-%d_%H-%M-%S`
 
-ffmpeg -i $recordurl -b:v 2M -f flv -t $duration $location$cameraname-$date.flv
+ffmpeg -i $recordurl -b:v 2M -f flv -t $duration $location$date_$cameraname.flv
